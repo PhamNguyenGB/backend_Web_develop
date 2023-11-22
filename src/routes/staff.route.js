7c4d32ce6ee07528c052f32e089c3b9a90770a05
Pham Nguyen Gia Bao <phambao091@gmail.com>
2023-11-22 19:07:11 +0700

    update

--- src/routes/staff.route.js
+++ src/routes/staff.route.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const staffController = require('../controllers/staff.controller');
 // const { veryfyToken } = require('../middlewares/jsonAction.middleware');
-// const { veryfyToken } = require("../middlewares/auth.middleware")
+const { veryfyToken } = require("../middlewares/auth.middleware")
 
 const staffAuthRoute = express.Router();
 
 staffAuthRoute
     .post('/admin', staffController.loginStaff)
-// .post('/admin/register', staffController.registerStaff)
-// .post('/admin/logout', veryfyToken('staff'), staffController.logoutStaff)
+    .post('/admin/register', staffController.registerStaff)
+    .post('/admin/logout', veryfyToken('staff'), staffController.logoutStaff)
 
 
 module.exports = staffAuthRoute;
